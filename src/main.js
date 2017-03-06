719e653ad2891c7ac2bc6af8476c5efe6f481e0a
Dominic-Pace <dpace@nhpace.net>
2017-03-05 23:20:16 -0500

    Added functionality for user to create a new task to the current state.

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
 
 module.exports = React.createClass({
   getInitialState() {
     return({
-      tasks: ['Take out the trash', 'Get Groceries', 'Practice Piano']
+      tasks: ['Take out the trash', 'Get Groceries', 'Practice Piano'],
+      task: ''
     })
   },
 
@@ -27,12 +29,26 @@ module.exports = React.createClass({
     )
   },
 
+  addTask() {
+    let tasks = this.state.tasks.concat([this.state.task]);
+    this.setState({tasks: tasks})
+  },
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>
           To-Do Master
         </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Add a task...'
+          onChangeText={(text) => {
+            this.setState({task: text})
+            console.log(this.state.task);
+          }}
+          onEndEditing={()=>this.addTask()}
+        />
         {this.renderList(this.state.tasks)}
       </View>
     )
@@ -53,10 +69,18 @@ const styles = StyleSheet.create({
 
   task: {
     height: 60,
-    borderWidth: 1,
+    borderBottomWidth: 1,
     borderColor: 'black',
     justifyContent: 'center',
     alignItems: 'center'
+  },
 
+  input: {
+    height: 60,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'black',
+    textAlign: 'center',
+    margin: 10
   }
 })
